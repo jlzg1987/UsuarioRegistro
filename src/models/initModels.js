35d41994b9c04c194d933b215dc42e15bed62b63
jlzg1987 <jlzg1987@hotmail.com>
2023-08-03 15:59:11 -0500

    update

---
+++ src/models/initModels.js
@@ -0,0 +1,7 @@
+const User = require('./users.models')
+
+const initModels = () => {
+    User
+
+}
+module.exports = initModels;
---
+++ src/models/users.models.js
@@ -0,0 +1,27 @@
+const db = require('../utils/database')
+const { DataTypes } = require('sequelize')
+const User = db.define("user", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+
+    }
+});
+module.exports = User;
